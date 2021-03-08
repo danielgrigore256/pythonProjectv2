d66f629b72e5652654ed390234026a7e16ce4a57
danielgrigore256 <grigore.daniel.256@gmail.com>
2021-03-08 12:13:03 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,270 @@
+import json
+jay= {
+    "totalCount": "5",
+    "imdata": [
+        {
+            "hcloudCtx": {
+                "attributes": {
+                    "awsVPC": "",
+                    "azResourceGroup": "",
+                    "azVirtualNetwork": "",
+                    "childAction": "",
+                    "ctxProfileName": "ct_ctxprofile_us-west-1",
+                    "delegateDn": "uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1",
+                    "description": "",
+                    "dn": "acct-[infra]/region-[us-west-1]/context-[overlay-1]-addr-[10.10.0.0/25]",
+                    "encap": "16777199",
+                    "encapType": "vxlan",
+                    "fvCtxDn": "uni/tn-infra/ctx-overlay-1",
+                    "interSitePeeringEnabled": "yes",
+                    "lcOwn": "local",
+                    "modTs": "2020-10-22T16:27:14.966+00:00",
+                    "name": "overlay-1",
+                    "nameAlias": "",
+                    "primaryCidr": "10.10.0.0/25",
+                    "resolvedObjDn": "ctxdefcont/ctxProfileVrfDef-[uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1]-ctxDef-[uni/tn-infra/ctx-overlay-1]",
+                    "status": "",
+                    "tenantName": "infra",
+                    "type": "regular"
+                },
+                "children": [
+                    {
+                        "healthInst": {
+                            "attributes": {
+                                "childAction": "",
+                                "chng": "0",
+                                "cur": "80",
+                                "maxSev": "cleared",
+                                "prev": "100",
+                                "rn": "health",
+                                "status": "",
+                                "twScore": "100",
+                                "updTs": "2020-10-27T12:46:50.008+00:00"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "hcloudCtx": {
+                "attributes": {
+                    "awsVPC": "",
+                    "azResourceGroup": "",
+                    "azVirtualNetwork": "",
+                    "childAction": "",
+                    "ctxProfileName": "ct_ctxprofile_us-west-1",
+                    "delegateDn": "uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1",
+                    "description": "",
+                    "dn": "acct-[infra]/region-[us-west-1]/context-[overlay-1]-addr-[10.10.0.0/25]",
+                    "encap": "16777199",
+                    "encapType": "vxlan",
+                    "fvCtxDn": "uni/tn-infra/ctx-overlay-1",
+                    "interSitePeeringEnabled": "yes",
+                    "lcOwn": "local",
+                    "modTs": "2020-10-20T16:27:14.966+00:00",
+                    "name": "overlay-1",
+                    "nameAlias": "",
+                    "primaryCidr": "10.10.0.0/25",
+                    "resolvedObjDn": "ctxdefcont/ctxProfileVrfDef-[uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1]-ctxDef-[uni/tn-infra/ctx-overlay-1]",
+                    "status": "",
+                    "tenantName": "infra",
+                    "type": "regular"
+                },
+                "children": [
+                    {
+                        "healthInst": {
+                            "attributes": {
+                                "childAction": "",
+                                "chng": "0",
+                                "cur": "70",
+                                "maxSev": "cleared",
+                                "prev": "100",
+                                "rn": "health",
+                                "status": "",
+                                "twScore": "100",
+                                "updTs": "2020-10-27T12:46:50.008+00:00"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "hcloudCtx": {
+                "attributes": {
+                    "awsVPC": "",
+                    "azResourceGroup": "",
+                    "azVirtualNetwork": "",
+                    "childAction": "",
+                    "ctxProfileName": "ct_ctxprofile_us-west-1",
+                    "delegateDn": "uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1",
+                    "description": "",
+                    "dn": "acct-[infra]/region-[us-west-1]/context-[overlay-1]-addr-[10.10.0.0/25]",
+                    "encap": "16777199",
+                    "encapType": "vxlan",
+                    "fvCtxDn": "uni/tn-infra/ctx-overlay-1",
+                    "interSitePeeringEnabled": "yes",
+                    "lcOwn": "local",
+                    "modTs": "2020-11-22T11:27:14.966+00:00",
+                    "name": "overlay-1",
+                    "nameAlias": "",
+                    "primaryCidr": "10.10.0.0/25",
+                    "resolvedObjDn": "ctxdefcont/ctxProfileVrfDef-[uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-1]-ctxDef-[uni/tn-infra/ctx-overlay-1]",
+                    "status": "",
+                    "tenantName": "infra",
+                    "type": "regular"
+                },
+                "children": [
+                    {
+                        "healthInst": {
+                            "attributes": {
+                                "childAction": "",
+                                "chng": "0",
+                                "cur": "87",
+                                "maxSev": "cleared",
+                                "prev": "100",
+                                "rn": "health",
+                                "status": "",
+                                "twScore": "100",
+                                "updTs": "2020-10-27T12:46:50.008+00:00"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "hcloudCtx": {
+                "attributes": {
+                    "awsVPC": "",
+                    "azResourceGroup": "",
+                    "azVirtualNetwork": "",
+                    "childAction": "",
+                    "ctxProfileName": "CLD_CTX_PROF_FOR_SG",
+                    "delegateDn": "uni/tn-Tenant_sg/ctxprofile-CLD_CTX_PROF_FOR_SG",
+                    "description": "",
+                    "dn": "acct-[Tenant_sg]/region-[us-west-1]/context-[Vrf_sg]-addr-[20.20.0.0/16]",
+                    "encap": "2719747",
+                    "encapType": "vxlan",
+                    "fvCtxDn": "uni/tn-Tenant_sg/ctx-Vrf_sg",
+                    "interSitePeeringEnabled": "no",
+                    "lcOwn": "local",
+                    "modTs": "2020-10-26T13:31:18.280+00:00",
+                    "name": "Vrf_sg",
+                    "nameAlias": "",
+                    "primaryCidr": "20.20.0.0/16",
+                    "resolvedObjDn": "ctxdefcont/ctxProfileVrfDef-[uni/tn-Tenant_sg/ctxprofile-CLD_CTX_PROF_FOR_SG]-ctxDef-[uni/tn-Tenant_sg/ctx-Vrf_sg]",
+                    "status": "",
+                    "tenantName": "Tenant_sg",
+                    "type": "regular"
+                },
+                "children": [
+                    {
+                        "healthInst": {
+                            "attributes": {
+                                "childAction": "",
+                                "chng": "0",
+                                "cur": "100",
+                                "maxSev": "cleared",
+                                "prev": "100",
+                                "rn": "health",
+                                "status": "",
+                                "twScore": "100",
+                                "updTs": "2020-10-27T12:46:50.008+00:00"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            "hcloudCtx": {
+                "attributes": {
+                    "awsVPC": "",
+                    "azResourceGroup": "",
+                    "azVirtualNetwork": "",
+                    "childAction": "",
+                    "ctxProfileName": "ct_ctxprofile_us-west-2",
+                    "delegateDn": "uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-2",
+                    "description": "",
+                    "dn": "acct-[infra]/region-[us-west-2]/context-[overlay-1]-addr-[11.11.11.0/25]",
+                    "encap": "16777199",
+                    "encapType": "vxlan",
+                    "fvCtxDn": "uni/tn-infra/ctx-overlay-1",
+                    "interSitePeeringEnabled": "yes",
+                    "lcOwn": "local",
+                    "modTs": "2020-10-27T09:45:41.089+00:00",
+                    "name": "overlay-1",
+                    "nameAlias": "",
+                    "primaryCidr": "11.11.11.0/25",
+                    "resolvedObjDn": "ctxdefcont/ctxProfileVrfDef-[uni/tn-infra/ctxprofile-ct_ctxprofile_us-west-2]-ctxDef-[uni/tn-infra/ctx-overlay-1]",
+                    "status": "",
+                    "tenantName": "infra",
+                    "type": "regular"
+                },
+                "children": [
+                    {
+                        "healthInst": {
+                            "attributes": {
+                                "childAction": "",
+                                "chng": "0",
+                                "cur": "12",
+                                "maxSev": "cleared",
+                                "prev": "100",
+                                "rn": "health",
+                                "status": "",
+                                "twScore": "100",
+                                "updTs": "2020-10-27T12:46:50.008+00:00"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+
+class HealthInst :
+    counter2 = 0
+    def __init__(self):
+        j = int(HealthInst.counter2)
+        self.current_health = jay["imdata"][j]["hcloudCtx"]["children"]["healthInst"]["attributes"]["cur"]
+        self.max_sev = jay["imdata"][j]["hcloudCtx"]["children"]["healthInst"]["attributes"]["maxSev"]
+        HealthInst.counter2 += 1
+    def displayed_health(self):
+        if int(self.current_health) == 100 :
+            print ("Healthy")
+        else :
+            print ("Unhealthy")
+
+class CloudCtx :
+    counter = 0
+    def __init__(self):
+        i = CloudCtx.counter
+        self.name = jay["imdata"][i]["hcloudCtx"]["attributes"]["name"]
+        if self.name == "" :
+            self.name = "-"
+        self.tenant_name = jay["imdata"][i]["hcloudCtx"]["attributes"]["tenantName"]
+        if self.tenant_name == "" :
+            self.tenant_name ="-"
+        self.description = jay["imdata"][i]["hcloudCtx"]["attributes"]["description"]
+        if self.description == "" :
+            self.description = "-"
+        self.name_alias = jay["imdata"][i]["hcloudCtx"]["attributes"]["nameAlias"]
+        if self.name_alias =="" :
+            self.name_alias = "-"
+        self.ctx_profile_name = jay["imdata"][i]["hcloudCtx"]["attributes"]["ctxProfileName"]
+        if self.ctx_profile_name =="" :
+            self.ctx_profile_name = "-"
+        CloudCtx.counter += 1
+    def displayCtx (self):
+        print("Name:",self.name)
+        print("Tenant_ame:",self.tenant_name)
+        print("Description", self.description)
+        print("Name_alias:", self.name_alias)
+
+        print("CTX Profile Nmae:", self.ctx_profile_name)
+
+
