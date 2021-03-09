8b2907a5ec5e00efef89337dd58624fb4e6245f7
danielgrigore256 <grigore.daniel.256@gmail.com>
2021-03-09 12:51:45 +0200

    Update on requirements

--- main.py
+++ main.py
@@ -1,90 +1,54 @@
 import json
 
-
+# importing the contents of JSON file into the program
 jay = json.load(open(r'C:\Users\DGrigore\AppData\Roaming\JetBrains\PyCharmCE2020.3\scratches\scratch.json5', 'r'))
 
 
-class HealthInst:
-    counter2 = 0
-
-    def __init__(self):
-
-        """
-        __init__ method : adds into the instanced object attributes from JSON file
-            param current_health : cur attribute from JSON file
-                type current_health : int
-            param max_sev: maxSev attribute from JSON file
-                type max_sev : string
-        """
-        j = int(HealthInst.counter2)
-        self.current_health = int(jay["imdata"][j]["hcloudCtx"]["children"][0]["healthInst"]["attributes"]["cur"])
-        self.max_sev = jay["imdata"][j]["hcloudCtx"]["children"][0]["healthInst"]["attributes"]["maxSev"]
-        HealthInst.counter2 += 1
-
-    def displayed_health(self):
-
-        """
-
-        compares the value of current_health attribute to 100
-        :return: a string
-
-        """
-        if int(self.current_health) == 100:
-            print("Healthy")
-        else:
-            print("Unhealthy")
-
-
-class CloudCtx(HealthInst):
+class CloudCtx:
     counter = 0
 
-    def __init__(self):
+    def __init__(self, name, tenant_name, description, name_alias, ctx_profile_name, displayed_health):
         """
 
-        __init__ method : adds into the instanced object attributes from JSON file
+               __init__ method : adds into the instanced object attributes from JSON file
 
-        param name : name attribute
-            type name : string
-        param tenant_name : tenant name
-            type tenant_name : string
-        param description : description attribute
-            type description : string
-        parm name_alias : name alias attribute
-            type name_alias : string
-        super().__init__ : calls the initialization method of the parent class HealthInst
-        take_from_jay : method to access the json file and index the info to object attribute
+               :param name : name attribute
+                   :type name : string
+               :param tenant_name : tenant name
+                   :type tenant_name : string
+               :param description : description attribute
+                   :type description : string
+               :parm name_alias : name alias attribute
+                   :type name_alias : string
+               :param ctx_profile_name : ctx profile name
+                   :type : string
+
+                using checkifnull() method we check if entry is empty ("")
+                    :return  replace empty records with ("-")
 
 
         """
-        self.name = CloudCtx.take_from_jay(CloudCtx.counter, "name")
-        self.tenant_name = CloudCtx.take_from_jay(CloudCtx.counter, "tenantName")
-        self.description = CloudCtx.take_from_jay(CloudCtx.counter, "description")
-        self.name_alias = CloudCtx.take_from_jay(CloudCtx.counter, "nameAlias")
-        self.ctx_profile_name = CloudCtx.take_from_jay(CloudCtx.counter, "ctxProfileName")
+        self.name = name
+        self.name = CloudCtx.checkifnull(self.name)
+        self.tenant_name = tenant_name
+        self.tenant_name = CloudCtx.checkifnull(self.tenant_name)
+        self.description = description
+        self.description = CloudCtx.checkifnull(self.description)
+        self.name_alias = name_alias
+        self.name_alias = CloudCtx.checkifnull(self.name_alias)
+        self.ctx_profile_name = ctx_profile_name
+        self.ctx_profile_name = CloudCtx.checkifnull(self.ctx_profile_name)
+
+        """
+            extra values taken from Healthinst class (method displayed_health)
+                :param : displayed_health : enum "Healthy"/"Unhealthy"
+                    :type : string
+        """
+        self.displayed_health = displayed_health
 
         CloudCtx.counter += 1
 
-        super().__init__()
-
-    def take_from_jay(j, cheie):
-
-        """
-        :param j : index, connected to the number of entries, needed to index the JSON file
-            param type : int
-        :param cheie: Index key that is given to to acces the the json entry that suits the attribute
-            param type : str
-        :return: the information (data) that needs to be stored in the object attribute
-        param type : str
-
-        """
-
-        temp = jay["imdata"][j]["hcloudCtx"]["attributes"][cheie]
-        if temp == "":
-            temp = "-"
-        return temp
-
     def displayctx(self):
-
         """
         method to display on console all values of the object
 
@@ -93,44 +57,102 @@ class CloudCtx(HealthInst):
         print("Tenant_ame:", self.tenant_name)
         print("Description", self.description)
         print("Name_alias:", self.name_alias)
-        print("CTX Profile Nmae:", self.ctx_profile_name)
+        print("CTX Profile Name:", self.ctx_profile_name)
+        print("Health Status:", self.displayed_health)
 
-    def health(self):
+    def checkifnull(attribute):
+        """
+        check if Cloud Ctx attribute is null ( = "" )
+        :return: new attribute value ("-")
+        """
+        if attribute == "":
+            attribute = "-"
+        return attribute
+
+
+class HealthInst:
+
+    """
+    class variables :
+        :param counter2 : increments at each new instance created
+            :type : int
+        :param healthlist : list that stores all current_health values
+            :type : list
+    """
+    counter2 = 0
+    healthlist = []
+
+    def __init__(self, current_health, max_sev):
 
         """
-        calling the displayed_health method of parent class,HealthInst()
+        __init__ method : adds into the instanced object attributes from JSON file
+            param current_health : cur attribute from JSON file
+                type current_health : int
+            param max_sev: maxSev attribute from JSON file
+                type max_sev : string
+        """
+
+        self.current_health = current_health
+        self.max_sev = max_sev
+
+        HealthInst.counter2 += 1
+        HealthInst.healthlist.append(self.current_health)
+
+    def displayed_health(self):
 
         """
-        super().displayed_health()
+        compares the value of current_health attribute to 100
+        :return: a string
+        """
+
+        if int(self.current_health) == 100:
+            return "Healthy"
+        else:
+            return "Unhealthy"
+
+    def order_health():
+        """
+        orders the current health values of the Healthinst objects attributes
+        :return: prints the lowest to highest value (of health ) in a sorted healthlist[object.current_health]
+        """
+
+        for index in sorted(HealthInst.healthlist):
+            print(index)
 
 
 """
-
-# creating objects that use CloudCtx and Healthinst values from json
-ctx1 = CloudCtx()
-ctx2 = CloudCtx()
-ctx3 = CloudCtx()
-
-
-print(ctx1.name, ctx1.tenant_name, ctx1.current_health)
-print(ctx2.name, ctx2.tenant_name, ctx2.current_health)
-print(ctx3.name, ctx3.tenant_name, ctx3.current_health)
-
-# sorting objects by current health
-order = sorted([int(ctx1.current_health), int(ctx2.current_health), int(ctx3.current_health)])
-for x in order:
-    print(x)
-# keeping track of objects
-print("We got", CloudCtx.counter, "objects")
+Creating the loop to parse the json file and add contents from it to our classes
+Using variables like get_(attribute_name) to temporary store data from json and move to object's attribute
+ 
 """
-ctx1 = CloudCtx()
-ctx2 = CloudCtx()
-ctx3 = CloudCtx()
-ctx4 = CloudCtx()
-ctx5 = CloudCtx()
+# creating two lists that will hold our objects , one for each class
+cloudctx_obj_list = []
+healthinst_obj_list = []
 
-ctx1.displayctx()
-ctx2.displayctx()
-ctx3.displayctx()
-ctx4.displayctx()
-ctx5.displayctx()
+for i in range(int(jay["totalCount"])):
+
+    # getting Healthinst attributes
+    get_current_health = int(jay["imdata"][i]["hcloudCtx"]["children"][0]["healthInst"]["attributes"]["cur"])
+    get_max_sev = jay["imdata"][i]["hcloudCtx"]["children"][0]["healthInst"]["attributes"]["maxSev"]
+
+    # getting Cloud Ctx attributes
+    get_name = jay["imdata"][i]["hcloudCtx"]["attributes"]["name"]
+    get_tenant_name = jay["imdata"][i]["hcloudCtx"]["attributes"]["tenantName"]
+    get_description = jay["imdata"][i]["hcloudCtx"]["attributes"]["description"]
+    get_name_alias = jay["imdata"][i]["hcloudCtx"]["attributes"]["nameAlias"]
+    get_ctx_profile_name = jay["imdata"][i]["hcloudCtx"]["attributes"]["ctxProfileName"]
+
+    # Creating temporary objects to store data that position "i"
+    obj_healthinst = HealthInst(get_current_health, get_max_sev)
+    obj_cloudctx = CloudCtx(get_name, get_tenant_name, get_description,
+                            get_name_alias, get_ctx_profile_name, obj_healthinst.displayed_health())
+
+    # populating list with the instanced objects
+    healthinst_obj_list.append(obj_healthinst)
+    cloudctx_obj_list.append(obj_cloudctx)
+
+# 11. Display objects in order
+HealthInst.order_health()
+
+# 12. Keep track of the number of objects
+print("There are", CloudCtx.counter, "objects created")
